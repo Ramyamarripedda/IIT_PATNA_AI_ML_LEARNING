97da8fab483e628a66a8e3386723ceb62f9c5748
Ramyamarripedda <sahasrapramod@gmail.com>
2026-04-09 12:44:44 +0530

    first commit

--- day_1.py
+++ day_1.py
@@ -1,9 +1,86 @@
-a=10
-b=20
-c=a+b
-print(c)
+# data types in python (int, float, str, bool):
 
-x= 5
-y= 10
-z= 15
-print(x*y*z)
+# print("hello, all the best for learning python")  # string data type
+
+# a = 10   # integer data type
+# b = 30   # integer data type
+# c = a + b
+# print("total:" , c)
+
+# a = int(input("enter the first number:"))
+# b = int(input("enter the second number:"))
+# print("total:", a + b)
+
+# a = 100   # integer data type
+# print(float(a))
+
+# b = 85.9  # float data type
+# print(int(b))
+
+# name = "Ramya"
+# age = 35     # integer data type
+# hight = 5.5  # float data type
+# print(f"my name is {name}, my age is {age} and my hight is {hight}")
+
+# first_name = "Ramya"          # string data type  
+# last_name = "marripedda"      # string data type
+# print("welcome to python programming", first_name, last_name)
+# print(f"welcome to python programming {first_name} {last_name}")
+
+# item = "tea" # string data type
+# price = 10   # integer data type
+# gst = 0.18   # float data type
+# total_price = price + (price * gst)
+# print (f"item: {item} | price: {price} | gst: {gst} | total_price: {total_price}")
+
+# a = True  # boolean data type
+# b = False # boolean data type
+# print(a)
+# print(b)
+
+# print (5 > 3)  # true
+# print (7 < 2)  # false
+
+# print (10 == 10)
+# print (10 == 5)
+
+# Type conversion (casting): (int, float, str, bool)
+# int() ------> converts a value to an integer data type
+# float() -----> converts a value to a float data type
+# str() -------> converts a value to a string data type
+# bool() -------> converts a value to a boolean data type
+
+# marks_str = "95"        # string data type to integer data type
+# marks_int = int(marks_str)
+# print (type(marks_str))
+# print (type(marks_int))
+# print(marks_int)
+
+# marks_str = "93.9"      # string data type to float data type
+# marks_float = float(marks_str)
+# print (type(marks_str))
+# print (type(marks_float))
+# print (marks_float)
+
+# my_name = "Ramya"       # string data type to boolean data type
+# name_bool = bool(my_name)
+# print (type(my_name))
+# print (type(name_bool))
+# print (name_bool)
+
+# my_age = 35      # integer data type to string data type
+# message = "i am ramya and i am" + " " + str(my_age) + " " + "years old"
+# print (message)
+
+# marks_int = 95   # integer data type to float data type
+# marks_float = float(marks_int)
+# print(type(marks_int))
+# print(type(marks_float))
+# print(marks_float)
+
+# bill_float = 150.75   # float data type to integer data type
+# bill_int = int(bill_float)
+# print(type(bill_float))
+# print(type(bill_int))
+# print(bill_int)
+
